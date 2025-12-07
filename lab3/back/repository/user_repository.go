f2e482ccdabe8fcf19e2607ef93bc50674b783ae
TitleHHHH <TitleHHHH@outlook.com>
2025-12-07 16:08:40 +0500

    add methods for save and load

--- lab3/back/repository/user_repository.go
+++ lab3/back/repository/user_repository.go
@@ -19,13 +19,14 @@ func (u *userRepository) AddUser(user models.User) error {
 	u.mu.Lock()
 	defer u.mu.Unlock()
 
-	u.users = append(u.users, user)
-	data, err := json.MarshalIndent(u.users, "", "\t")
-	if err != nil {
-		return err
+	user1, _ := u.GetByUsernameUnsafe(user.Username)
+	if user1 != nil {
+		return errors.New("user already exists")
 	}
 
-	return os.WriteFile(fileName, data, 0644)
+	u.users = append(u.users, user)
+
+	return saveUsers(fileName, u.users)
 }
 
 func (u *userRepository) RemoveUser(id int) error {
@@ -33,9 +34,7 @@ func (u *userRepository) RemoveUser(id int) error {
 	panic("implement me")
 }
 
-func (u *userRepository) GetByUsername(username string) (*models.User, error) {
-	u.mu.Lock()
-	defer u.mu.Unlock()
+func (u *userRepository) GetByUsernameUnsafe(username string) (*models.User, error) {
 	for _, user := range u.users {
 		if user.Username == username {
 			return &user, nil
@@ -44,6 +43,49 @@ func (u *userRepository) GetByUsername(username string) (*models.User, error) {
 	return nil, errors.New("user not found")
 }
 
+func (u *userRepository) GetByUsername(username string) (*models.User, error) {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+	return u.GetByUsernameUnsafe(username)
+}
+
+func saveUsers(path string, users []models.User) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+	return encoder.Encode(users)
+}
+
+func loadUsers(path string) ([]models.User, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var users []models.User
+	err = json.NewDecoder(file).Decode(&users)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
 func NewUserRepository() UserRepository {
-	return &userRepository{}
+
+	users, err := loadUsers(fileName)
+
+	if err != nil {
+		users = []models.User{}
+	}
+
+	return &userRepository{
+		users: users,
+	}
 }
